88640fc4c5bcfd0aa9c87428ec03307048b3a8ed
rubendcode <rubendijksma@gmail.com>
2024-05-19 20:16:41 +0200

    new
    new changes

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("scavengerhunt.html")
+
+@app.route("/missions")
+def missions():
+    return render_template("missions.html")
+
+if __name__ == "__main__":
+    app.run()
